3d046e017949a17aa09598ccdf45ed584660dd71
thaffenden <tristanehaffenden@gmail.com>
2018-01-10 12:44:36 +0000

    Updates install script to also enable the backlight when called

--- install.sh
+++ install.sh
@@ -6,11 +6,17 @@ function set_backlight_file_path_in_desktop_file() {
     echo '.desktop file path set'
 }
 
-function enable_kb_backlight() {
+function enable_kb_backlight_at_start_up() {
     ln -s $PWD/.desktop $HOME/.config/autostart/.desktop
     echo 'Keyboard backlight enabled at start up'	
 }
 
+function turn_on_kb_backlight() {
+    sh ./kb_backlight.sh
+    echo 'Keyboard backlight turned on'
+}
+
 set_backlight_file_path_in_desktop_file
-enable_kb_backlight
+enable_kb_backlight_at_start_up
+turn_on_kb_backlight
 

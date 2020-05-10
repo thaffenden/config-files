769e9d16e4e79a3d8d4fea7af893f887a3df4886
Tristan Haffenden <tristanehaffenden+github@gmail.com>
2020-05-10 17:37:35 +0100

    Update tools install file

--- install/tools.sh
+++ install/tools.sh
@@ -4,12 +4,18 @@ function install_alacritty() {
   sudo apt install alacritty
 }
 
-function install_node_version_manager() {
-  curl -fsSL https://github.com/Schniz/fnm/raw/master/.ci/install.sh | bash
+function install_bat() {
+  bat_version="0.15.0"
+  bat_package_name="bat_" + $bat_version + "_amd64.deb"
+  curl -LO https://github.com/sharkdp/bat/releases/download/v$bat_version/$bat && sudo dpkg -i $bat_package_name && rm $bat_package_name
 }
 
-function install_bat() {
-  sudo apt install bat
+function install_jq() {
+  sudo apt-get install jq
+}
+
+function install_node_version_manager() {
+  curl -fsSL https://github.com/Schniz/fnm/raw/master/.ci/install.sh | bash
 }
 
 function install_ripgrep() {
@@ -18,11 +24,12 @@ function install_ripgrep() {
   curl -LO https://github.com/BurntSushi/ripgrep/releases/download/$rg_version/$rg_package_name && sudo dpkg -i $rg_package_name && rm $rg_package_name
 }
 
-function install_jq() {
-  sudo apt-get install jq
+function install_tmux() {
+  sudo apt install tmux
 }
 
 install_node_version_manager
 install_ripgrep
 install_bat
 install_jq
+install_tmux

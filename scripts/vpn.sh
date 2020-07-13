1682723ba042357b23464a01d89ccfdb81aaf465
Tristan Haffenden <tristanehaffenden+github@gmail.com>
2020-07-13 08:45:47 +0100

    switch case structure

--- scripts/vpn.sh
+++ scripts/vpn.sh
@@ -1,1 +1,20 @@
+#!/bin/sh
 export VPN_SCRIPT='true'
+
+vpn_command() {
+  case $1 in
+    home)
+      echo "home"
+      ;;
+
+    work)
+      echo "work"
+      ;;
+
+    *)
+      echo "unrecognised arguments $@ passed to VPN command"
+      ;;
+  esac
+}
+
+alias vpn=vpn_command $@

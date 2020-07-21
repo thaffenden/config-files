704cdcad84c19acbcfe4ef92f9a68bc9169d63e8
Tristan Haffenden <tristanehaffenden+github@gmail.com>
2020-07-21 21:26:16 +0100

    Improve output of now playing script

--- i3/polybar/scripts/now_playing.sh
+++ i3/polybar/scripts/now_playing.sh
@@ -1,10 +1,28 @@
 #! /bin/bash
-players=`playerctl --list-all`
+json=`playerctl metadata --format '{"artist":"{{ artist }}","title":"{{ title }}","album":"{{ album }}","player":"{{ playerName }}"}' | jq .`
+player=`jq '.player' <<< $json | awk -F'"' '$0=$2'`
 
-if [[ $players == *"No players were found"* ]]; then
-  echo ""
-else
-  echo `playerctl metadata --format '{{ artist }} - {{ title }} [{{ album }}]'`
-fi
+case "$player" in
+  "firefox")
+    title=`jq '.title' <<< $json`
+    artist=$(echo "$title" | awk -F'"' '$0=$2' | awk -F' - ' '$0=$1')
+    song=$(echo "$title" | awk -F'"' '$0=$2' | awk -F' - ' '$0=$2')
 
+    case "$song" in
+      "YouTube")
+        echo " $artist"
+        ;;
+
+      *)
+        echo "$artist - $song"
+        ;;
+    esac
+    ;;
+
+  *)
+    echo "$player"
+    # echo `playerctl metadata --format '{"artist":"{{ artist }}","title":"{{ title }}","album":"{{ album }}","player":"{{ playerName }}"}'`
+    ;;
+
+esac
 # might need custom handling for firefox not outputting data.

cf2a755c3a3e88d18635e35d4784454384808652
Tristan Haffenden <tristanehaffenden+github@gmail.com>
2020-07-26 15:47:00 +0100

    add initial i3 set up script

---
+++ install/i3-setup.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+function install_core_deps() {
+  sudo add-apt-repository ppa:kgilmer/speed-ricer
+  sudo apt update
+
+  sudo apt install i3-gaps-wm polybar rofi compton feh
+}
+
+function symlink_config() {
+  ln -s $HOME/git/config-files/i3 $HOME/.config/i3
+  ln -s $HOME/git/config-files/polybar $HOME/.config/polybar
+  ln -s $HOME/git/config-files/compton/compton.conf $HOME/.config/compton.conf
+  ln -s $HOME/git/config-files/rofi $HOME/.config/rofi
+}
+
+install_core_deps
+symlink_config

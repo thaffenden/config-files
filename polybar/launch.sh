ef4c874e60576cd389080fc6f5f9fb1b63017d6d
Tristan Haffenden <tristanehaffenden+github@gmail.com>
2020-07-27 09:18:34 +0100

    launch polybar via script

---
+++ polybar/launch.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Launch bar1 and bar2
+echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+polybar top >>/tmp/polybar1.log 2>&1 &
+
+echo "Bar launched..."

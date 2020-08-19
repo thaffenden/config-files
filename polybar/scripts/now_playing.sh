75fab3e3be30d6255f489c7f947201f2ac18ba47
Tristan Haffenden <tristanehaffenden+github@gmail.com>
2020-08-19 15:27:56 +0100

    strip play symbol off firefox now playing in polybar

--- polybar/scripts/now_playing.sh
+++ polybar/scripts/now_playing.sh
@@ -1,25 +1,28 @@
 #! /bin/bash
 json=`playerctl metadata --format '{"artist":"{{ artist }}","title":"{{ title }}","album":"{{ album }}","player":"{{ playerName }}"}' | jq .`
-player=`jq '.player' <<< $json | awk -F'"' '$0=$2'`
+player=`jq '.player' -r <<< $json`
 
 case "$player" in
   "firefox")
-    title=`jq '.title' <<< $json | awk -F'"' '$0=$2'`
+    playChar="▶ "
+    title=`jq '.title' -r <<< $json`
+    title=${title#"$playChar"}
     artist=$(echo "$title" | awk -F' - ' '$0=$1')
 
     if [[ "${title##* }" == "YouTube" ]]; then
       echo '%{F#FF5252}%{T3}%{F- T-}' "$(echo $title | awk 'BEGIN { ORS=" " }; {n=split($0,A," - "); for (i = 1; i <= n - 1; i++) print A[i]}')"
-      break;
+      exit 0;
     fi
 
     song=$(echo "$title" | awk -F' - ' '$0=$2')
     if [[ "$song" != "" ]]; then
       echo '%{F#FFD740}%{T3}﮸%{F- T-}' "$artist - $song"
-      break;
+      exit 0;
     fi
 
     if [[ `echo $artist | awk -F' | ' '$0=$1'` == "Slack" ]]; then
       echo '%{F#FF4081}%{T3}%{F- T-}' "$artist"
+      exit 0;
     fi
 
     echo '%{F#FFD740}%{T3}?%{F- T-}' "$artist"

ed644be4a5a655e133085c32306ee3d9b64414f4
Tristan Haffenden <tristanehaffenden+github@gmail.com>
2020-08-20 05:57:27 +0100

    improve polybar RHS spacing

--- polybar/scripts/now_playing.sh
+++ polybar/scripts/now_playing.sh
@@ -10,28 +10,28 @@ case "$player" in
     artist=$(echo "$title" | awk -F' - ' '$0=$1')
 
     if [[ "${title##* }" == "YouTube" ]]; then
-      echo '%{F#FF5252}%{T3}%{F- T-}' "$(echo $title | awk 'BEGIN { ORS=" " }; {n=split($0,A," - "); for (i = 1; i <= n - 1; i++) print A[i]}')"
+      echo '%{F#FF5252}%{T3}%{F- T-}' "$(echo $title | awk 'BEGIN { ORS=" " }; {n=split($0,A," - "); for (i = 1; i <= n - 1; i++) print A[i]}')" '   '
       exit 0;
     fi
 
     song=$(echo "$title" | awk -F' - ' '$0=$2')
     if [[ "$song" != "" ]]; then
-      echo '%{F#FFD740}%{T3}﮸%{F- T-}' "$artist - $song"
+      echo '%{F#FFD740}%{T3}﮸%{F- T-}' "$artist - $song  "
       exit 0;
     fi
 
     if [[ `echo $artist | awk -F' | ' '$0=$1'` == "Slack" ]]; then
-      echo '%{F#FF4081}%{T3}%{F- T-}' "$artist"
+      echo '%{F#FF4081}%{T3}%{F- T-}' "$artist  "
       exit 0;
     fi
 
-    echo '%{F#FFD740}%{T3}?%{F- T-}' "$artist"
+    echo '%{F#FFD740}%{T3}?%{F- T-}' "$artist  "
     ;;
 
   "Plexamp")
     artist=`jq '.artist' <<< $json -r`
     song=`jq '.title' <<< $json -r`
-    echo '%{F#FFD740}%{T3}﮸%{F- T-}' "$artist - $song"
+    echo '%{F#FFD740}%{T3}﮸%{F- T-}' "$artist - $song  "
     ;;
 
   *)
--- polybar/scripts/vpn.sh
+++ polybar/scripts/vpn.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 if [[ `expressvpn status` != *"Not connected"* ]]; then
-  echo '%{T2}%{B#5CF19E}%{F#263238} vpn: %{T-}%{T4}A %{B- F- T-}'
+  echo '%{T2}%{B#5CF19E}%{F#263238} vpn: %{T-}%{T4}A  %{B- F- T-}'
 
 elif [[ `nmcli con show --active` == *"Vidsy Office"* ]]; then
-  echo '%{T2}%{B#FF0657} vpn: %{T-}%{T4}B %{B- F- T-}'
+  echo '%{T2}%{B#FF0657} vpn: %{T-}%{T4}B  %{B- F- T-}'
 
 else
-  echo '%{T2}%{B#FFD740}%{F#263238} vpn: none %{B- F- T-}'
+  echo '%{T2}%{B#FFD740}%{F#263238} vpn: none  %{B- F- T-}'
 fi

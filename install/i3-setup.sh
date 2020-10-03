6c96b433c395598dd79d5e239d1c96f88dfde577
Tristan Haffenden <tristanehaffenden+github@gmail.com>
2020-10-03 11:29:08 +0100

    add picom config

--- install/i3-setup.sh
+++ install/i3-setup.sh
@@ -9,6 +9,7 @@ function install_core_deps() {
 function symlink_config() {
   ln -s $HOME/git/config-files/i3 $HOME/.config/i3
   ln -s $HOME/git/config-files/polybar $HOME/.config/polybar
+  ln -s $HOME/git/config-files/picom $HOME/.config/picom
   ln -s $HOME/git/config-files/compton/compton.conf $HOME/.config/compton.conf
   ln -s $HOME/git/config-files/rofi $HOME/.config/rofi
   ln -s $HOME/git/config-files/dunst $HOME/.config/dunst

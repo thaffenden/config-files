4aa03f2b52b813add8440a32446dcd76d05ca3b7
Tristan Haffenden <tristanehaffenden+github@gmail.com>
2020-11-09 22:12:05 +0000

    update polybar config

--- polybar/scripts/vpn.sh
+++ polybar/scripts/vpn.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 if [[ `expressvpn status` != *"Not connected"* ]]; then
-  echo '%{T2}%{B#5CF19E}%{F#263238} vpn: %{T-}%{T4}B  %{B- F- T-}'
+  echo '%{F#5CF19E}%{T4}  B   %{F- T-}'
 
 elif [[ `nmcli con show --active` == *"Vidsy Office"* ]]; then
-  echo '%{T2}%{B#FF0657} vpn: %{T-}%{T4}C  %{B- F- T-}'
+  echo '%{F#FF0657}%{T4}  C   %{F- T-}'
 
 else
-  echo '%{T2}%{B#FFD740}%{F#263238} vpn: none  %{B- F- T-}'
+  echo '%{T2}%{F#FFD740} no vpn  %{F- T-}'
 fi
